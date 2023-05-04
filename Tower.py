a1ce19d4231118bfab9ba8604de2843892d61f23
Tal <leonardotal2005@gmail.com>
2023-05-04 13:19:27 +0300

    Original game done

--- Tower.py
+++ Tower.py
@@ -2,6 +2,8 @@ from Disk import Disk
 
 
 class Tower:
+    list_of_disks: list[Disk]
+
     def __init__(self, list_of_disks, x_pos):
         self.list_of_disks = list_of_disks
         self.x_pos = x_pos
@@ -10,11 +12,11 @@ class Tower:
         self.list_of_disks.append(disk)
 
     def remove_disk(self):
-        return self.list_of_disks.pop(-1)
+        return self.list_of_disks.pop()
 
     def get_amount_of_disks(self):
         return len(self.list_of_disks)
 
     def add_tower_to_screen(self):
-        for i in range(len(self.list_of_disks)):
-            Disk.draw_disk(self.list_of_disks[len(self.list_of_disks) - 1 - i], self.x_pos, i)
+        for i, disk in enumerate(self.list_of_disks):
+            disk.draw_disk(self.x_pos, i)
--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import pygame
+
 from constants import *
 from Disk import Disk
 from Tower import Tower
 
 
-def screen_output(tower_list, count):
+def screen_output(tower_list: list[Tower], count):
     screen.blit(background, (0, 0))
-    for i in range(len(tower_list)):
-        Tower.add_tower_to_screen(tower_list[i])
+    for tower in tower_list:
+        tower.add_tower_to_screen()
     font = pygame.font.SysFont("Aharoni", 20)
-    text_min_steps = font.render(f"Minimum Steps: {(2 ** disk_quantity) - 1}", True, (0, 0, 0))
+    text_min_steps = font.render(
+        f"Minimum Steps: {(2 ** DISK_QUANTITY) - 1}", True, (0, 0, 0)
+    )
     text_steps = font.render(f"Your Steps: {count+1}", True, (0, 0, 0))
     screen.blit(text_min_steps, (20, 20))
-    screen.blit(text_steps, (WINDOW_WIDTH-150, 20))
+    screen.blit(text_steps, (WINDOW_WIDTH - 150, 20))
 
 
 def hanoi(num_discs, source, target, auxiliary, step_list):
@@ -26,8 +29,8 @@ def hanoi(num_discs, source, target, auxiliary, step_list):
 
 
 def solve(tower_list, step_list, count):
-    source = tower_list[step_list[count][0]-1]
-    dest = tower_list[step_list[count][1]-1]
+    source = tower_list[step_list[count][0] - 1]
+    dest = tower_list[step_list[count][1] - 1]
     disk = source.remove_disk()
     dest.add_disk(disk)
     screen_output(tower_list, count)
@@ -35,46 +38,28 @@ def solve(tower_list, step_list, count):
 
 def main():
     pygame.init()
-    pygame.display.set_caption('Hanoi Towers')
+    pygame.display.set_caption("Hanoi Towers")
 
-    disk_list = []
+    disk_list = [
+        Disk(7, (0, 102, 0)),
+        Disk(6, (102, 0, 51)),
+        Disk(5, (255, 0, 255)),
+        Disk(4, (51, 51, 255)),
+        Disk(3, (255, 255, 0)),
+        Disk(2, (0, 255, 255)),
+        Disk(1, (255, 0, 0)),
+    ]
 
-    # size, color
-    d1 = Disk(1, (255, 0, 0))
-    d2 = Disk(2, (0, 255, 255))
-    d3 = Disk(3, (255, 255, 0))
-    d4 = Disk(4, (51, 51, 255))
-    d5 = Disk(5, (255, 0, 255))
-    d6 = Disk(6, (102, 0, 51))
-    d7 = Disk(7, (0, 102, 0))
+    disk_list = disk_list[len(disk_list) - DISK_QUANTITY :]
 
-    disk_list.append(d1)
-    disk_list.append(d2)
-    disk_list.append(d3)
-    disk_list.append(d4)
-    disk_list.append(d5)
-    disk_list.append(d6)
-    disk_list.append(d7)
-
-    tower_list = []
-
-    # list, x_pos
-    t1 = Tower(disk_list, 1)
-    t2 = Tower([], 2)
-    t3 = Tower([], 3)
-
-    tower_list.append(t1)
-    tower_list.append(t2)
-    tower_list.append(t3)
-
-    for i in range(7-disk_quantity):
-        del disk_list[-1]
+    tower_list = [Tower(disk_list, 1), Tower([], 2), Tower([], 3)]
 
     running = True
     step_list = []
     count = 0
-    hanoi(disk_quantity, 1, 3, 2, step_list)
+    hanoi(DISK_QUANTITY, 1, 3, 2, step_list)
     solved = False
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -84,7 +69,7 @@ def main():
         if not solved:
             solve(tower_list, step_list, count)
             count += 1
-            clock.tick(20)
+            clock.tick(3)
 
             if count == len(step_list):
                 solved = True
@@ -94,6 +79,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
